466f2e5540c23990e6175fedc7d138f41bbc0d0f
Tal Ashkenazi <78943831+tal-ashkenazi01@users.noreply.github.com>
2022-04-19 13:40:20 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ let screenWidth = 400; //copy paste this code at the top of every game to have u
 
 function preload() {
   // LOADING IN THE TEXTURE FOR THE PLANET
-  planet_texture = loadImage("/assets/planet_texture_map.jpeg");
+  planet_texture = loadImage("assets/planet_texture_map.jpeg");
   // LOAD IN THE FONT FOR THE TEXT
-  SpaceMono = loadFont("/assets/SpaceMono-Bold.ttf");
+  SpaceMono = loadFont("assets/SpaceMono-Bold.ttf");
 }
 
 function setup() {
