b5cb6d611b7d3c75d747487257a7da5c7b949af3
Tal Ashkenazi <78943831+tal-ashkenazi01@users.noreply.github.com>
2022-04-19 23:38:27 -0700

    Update mainMenu.js

--- mainMenu.js
+++ mainMenu.js
@@ -33,8 +33,6 @@ function setup() {
       ]);
     }
   }
-  // CANVAS POSITION IN THE MOMENT
-  let cnvPos = cnv.position();
 
   // CREATE BUTTONS FOR NAVIGATION
   MiningButton = createButton("Asteroid Mining");
