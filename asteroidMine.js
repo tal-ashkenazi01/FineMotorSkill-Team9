ee7df24434f4b26df61dfee16e3ff45aba8e1d14
Tal Ashkenazi <78943831+tal-ashkenazi01@users.noreply.github.com>
2022-04-19 18:31:18 -0700

    Update asteroidMine.js

--- asteroidMine.js
+++ asteroidMine.js
@@ -3,9 +3,9 @@ let coords = [];
 
 function preload() {
   // LOADING IN THE TEXTURE FOR THE PLANET
-  planet_texture = loadImage("/assets/planet_texture_map.jpeg");
+  planet_texture = loadImage("assets/planet_texture_map.jpeg");
   // LOAD IN THE FONT FOR THE TEXT
-  SpaceMono = loadFont("/assets/SpaceMono-Bold.ttf");
+  SpaceMono = loadFont("assets/SpaceMono-Bold.ttf");
 }
 
 function setup() {
